3c99254bed45aa6675ef20ec528c06b87e23ab3b
FRL-GQ128S2\Dominique <dominique.bureau@decathlon.com>
2021-02-14 10:05:50 +0100

    modif api router

--- public/js/main.js
+++ public/js/main.js
@@ -16385,7 +16385,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   methods: {
     logout: function logout() {
-      this.$store.dispatch('logout');
+      this.$store.dispatch("logout");
     }
   },
   computed: {
--- routes/api.php
+++ routes/api.php
@@ -25,8 +25,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-// Route::prefix('v1')->group(function () {
-Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function() {
+Route::prefix('v1')->group(function () {
+    Route::post('/login', 'App\Http\Controllers\AuthController@login')->name('login');
+    Route::post('/register', 'App\Http\Controllers\AuthController@register')->name('register');
+
     Route::apiResource('artists', ArtistController::class)->only(['index', 'show'])->whereUuid('artist');
     Route::apiResource('instruments', InstrumentController::class)->only(['index', 'show'])->whereUuid('instrument');
     Route::apiResource('bands', BandController::class)->only(['index', 'show'])->whereUuid('band');
@@ -35,13 +37,12 @@ Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function() {
     Route::apiResource('lyrics', LyricController::class)->only(['index', 'show'])->whereUuid('lyric');
 });
 
-Route::prefix('v1')->group(function () {
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function() {
     Route::apiResource('artists', ArtistController::class)->except(['index', 'show'])->whereUuid('artist');
 });
 
 
-Route::post('/login', 'App\Http\Controllers\AuthController@login')->name('login');
-Route::post('/register', 'App\Http\Controllers\AuthController@register')->name('register');
+
 Route::group(['middleware' => ['auth:sanctum']], function() {
     Route::post('/logout', 'App\Http\Controllers\AuthController@logout')->name('logout');
     Route::get('/events', 'App\Http\Controllers\EventController@list');
